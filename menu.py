ea3623e383f331ddaa39735fb1059461585a0cbd
Raimundo Navarro <raim.navarro@duocuc.cl>
2025-06-22 20:24:58 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,23 +1,2 @@
 def datos_raimundo():
  print("mi nombre es raimundo y tengo 19 años")
-while True:
- print("\n--- MENÚ PRINCIPAL ---")
- print("1. Función de integrante 1")
- print("2. Función de integrante 2")
- print("3. Función de integrante 3")
- print("4. Función de integrante 4")
- print("0. Salir")
- op = input("Seleccione opción: ")
- if op == "0":
- print("Programa finalizado.")
- break
- elif op == "1":
- pass # Aquí se llamará a la función del integrante 1
- elif op == "2":
- pass # Aquí se llamará a la función del integrante 2
- elif op == "3":# Aquí se llamará a la función del integrante 3
- pass
- elif op=="4:"# Aquí se llamará a la función del integrante 4
- datos_raimundo()
- else:
- print(" Opción inválida.")
