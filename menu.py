93829d2132a63ed937f02fb51ed488e7f579e6d8
Raimundo Navarro <raim.navarro@duocuc.cl>
2025-06-22 20:27:55 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,2 +1,22 @@
-def datos_raimundo():
- print("mi nombre es raimundo y tengo 19 años")
+def datos_raimund()
+print("Mi nombre es raimundo y tengo 19 años")
+while True:
+print("\n--- MENÚ PRINCIPAL ---")
+print("1. Función de integrante 1")
+print("2. Función de integrante 2")
+print("3. Función de integrante 3")
+print("0. Salir")
+op = input("Seleccione opción: ")
+if op == "0":
+print("Programa finalizado.")
+break
+elif op == "1":
+  datos_diego() 
+elif op == "2": 
+  datos_raimundo()
+elif op == "3": 
+  datos_santiago()
+elif op == "4":
+  datos_khristian()
+else:
+print("Opción inválida.")
