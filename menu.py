1875ab4339564052a023710fc618e54bad9bb79d
Khristian Acevedo <kh.acevedo@duocuc.cl>
2025-06-22 20:18:21 -0400

    Update.py

--- menu.py
+++ menu.py
@@ -1,21 +1,23 @@
-# Menú base del programa
+def datos_khristian():
+ print("Mi nombre es Khristian Acevedo y tengo 18 años.")
 while True:
-print("\n--- MENÚ PRINCIPAL ---")
-print("1. Función de integrante 1")
-print("2. Función de integrante 2")
-print("3. Función de integrante 3")
-print("0. Salir")
-op = input("Seleccione opción: ")
-if op == "0":
-print("Programa finalizado.")
-break
-elif op == "1":
-pass # Aquí se llamará a la función del integrante 1
-elif op == "2":
-pass # Aquí se llamará a la función del integrante 2
-elif op == "3":
-pass # Aquí se llamará a la función del integrante 3
-elif op == "4":
-pass
-else:
-print("Opción inválida.")
+ print("\n--- MENÚ PRINCIPAL ---")
+ print("1. Función de integrante 1")
+ print("2. Función de integrante 2")
+ print("3. Función de integrante 3")
+ print("4. Función de integrante 4")
+ print("0. Salir")
+ op = input("Seleccione opción: ")
+ if op == "0":
+ print("Programa finalizado.")
+ break
+ elif op == "1":
+ pass # Aquí se llamará a la función del integrante 1
+ elif op == "2":
+ pass # Aquí se llamará a la función del integrante 2
+ elif op == "3":# Aquí se llamará a la función del integrante 3
+ datos_khristian()
+ elif op=="4:"# Aquí se llamará a la función del integrante 4
+ pass
+ else:
+ print(" Opción inválida.")
